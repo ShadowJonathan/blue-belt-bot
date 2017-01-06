7f1653f557b7c7f415afd92497060f3d8c57e3c9
ShadowJonathan <jonathandejong02@gmail.com>
2017-01-06 19:40:11 +0100

    fixed invalid token reading

--- bot/bot.go
+++ bot/bot.go
@@ -4,10 +4,20 @@ import (
 	"io/ioutil"
 	"strings"
 
+	"fmt"
+
 	"../belter"
 )
 
 func main() {
-	token, _ := ioutil.ReadFile("token")
+	token, err := ioutil.ReadFile("../token")
+	if err != nil {
+		fmt.Println("Error reading token file: " + err.Error())
+		err := ioutil.WriteFile("token", token, 9000)
+		if err != nil {
+			fmt.Println("Error writing sample token file: " + err.Error())
+		}
+		return
+	}
 	Belt.Initialize(strings.TrimSpace(string(token)))
 }
