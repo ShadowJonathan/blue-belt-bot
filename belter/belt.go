c37666b4cc4382595077115c6187056e95b4826b
ShadowJonathan <jonathandejong02@gmail.com>
2017-01-06 15:50:05 +0100

    Added websocket handling

--- belter/belt.go
+++ belter/belt.go
@@ -3,6 +3,7 @@ package Belt
 import (
 	fmt "fmt"
 	"io/ioutil"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -19,26 +20,26 @@ type BlueBot struct {
 	dg      *discordgo.Session
 	Debug   bool
 	version Version
-    OwnID   string
-    OwnAV   string
-    OwnName string
+	OwnID   string
+	OwnAV   string
+	OwnName string
+	Stop    bool
 }
 
 // Vars after this
 
 var bbb *BlueBot
-var err error
 
-//Fuction of the bot, can be set to "bot" or "user"
+//BotFunc Fuction of the bot, can be set to "bot" or "user"
 var BotFunc string
 
 // Functions after this
 
 func BBReady(s *discordgo.Session, r *discordgo.Ready) {
-    bbb.OwnID = r.User.ID
-    bbb.OwnAV = r.User.Avatar
-    bbb.OwnName = r.User.Username
-    fmt.Println("Discord: Ready message received\nBB: I am '" + bbb.OwnName + "'!\nBB: My User ID: " + bbb.OwnID)
+	bbb.OwnID = r.User.ID
+	bbb.OwnAV = r.User.Avatar
+	bbb.OwnName = r.User.Username
+	fmt.Println("Discord: Ready message received\nBB: I am '" + bbb.OwnName + "'!\nBB: My User ID: " + bbb.OwnID)
 }
 
 func Initialize(Token string) {
@@ -59,6 +60,18 @@ func Initialize(Token string) {
 		fmt.Println("Discord Session error, check token, error message: " + err.Error())
 		return
 	}
+	// handlers
+	bbb.dg.AddHandler(BBReady)
 
-    sb.dg.AddHandler(BBReady)
+	err = bbb.dg.Open()
+	if err == nil {
+		fmt.Println("Discord: Connection established")
+		for !bbb.Stop {
+			time.Sleep(400 * time.Millisecond)
+		}
+	} else {
+		fmt.Println("Error opening websocket connection: ", err.Error())
+	}
+	fmt.Println("BBB: Blue belt stopping...")
+	bbb.dg.Close()
 }
