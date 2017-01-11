6f947998504874b9b25c1b3ff428ba0aa24a41f5
ShadowJonathan <jonathandejong02@gmail.com>
2017-01-11 10:42:58 +0100

    added URL downloader tool

--- belter/tools.go
+++ belter/tools.go
@@ -1,7 +1,36 @@
 package Belt
 
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
 func IsSpace(L byte) bool {
 	return L == ' ' || L == '\t' || L == '\r'
+}
+
+func DownloadUrl(URL string, filepath string) error {
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get
+	resp, err := http.Get(URL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func GetArgs(S string) []string {
